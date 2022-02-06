96a8ef0332d148718eb4a5b27b18a4c7f5b1e0ce
cfdol <99126330+cfdol@users.noreply.github.com>
2022-02-06 12:55:35 +0000

    Add files via upload

---
+++ AtrocityMedia/AtrocityMedia.lua
@@ -0,0 +1,16 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+	 
+--StatusBars
+LSM:Register("statusbar","Atrocity", [[Interface\Addons\AtrocityMedia\StatusBars\Atrocity]])
+LSM:Register("statusbar","Kait", [[Interface\Addons\AtrocityMedia\StatusBars\Kait.tga]])
+--Sounds
+LSM:Register("sound","Blade", [[Interface\Addons\AtrocityMedia\Sounds\Blade.ogg]])
+LSM:Register("sound","Buffed", [[Interface\Addons\AtrocityMedia\Sounds\Buffed.ogg]])
+LSM:Register("sound","Bullet", [[Interface\Addons\AtrocityMedia\Sounds\Bullet.ogg]])
+LSM:Register("sound","Info", [[Interface\Addons\AtrocityMedia\Sounds\Info.ogg]])
+LSM:Register("sound","Mail", [[Interface\Addons\AtrocityMedia\Sounds\Mail.ogg]])
+LSM:Register("sound","Positive", [[Interface\Addons\AtrocityMedia\Sounds\Positive.ogg]])
+LSM:Register("sound","Thunder", [[Interface\Addons\AtrocityMedia\Sounds\Thunder.ogg]])
+LSM:Register("sound","On You", [[Interface\Addons\AtrocityMedia\Sounds\OnYou.ogg]])
+--Fonts
+LSM:Register("font","Expressway", [[Interface\Addons\AtrocityMedia\Fonts\Expressway.TTF]])
---
+++ AtrocityMedia/Bindings.lua
@@ -0,0 +1,7 @@
+local GlobalAddonName, AUI = ...
+
+BINDING_HEADER_AUI = "AtrocityUI"
+BINDING_NAME_AUIHIDEBARS = "Hide ActionBars OOC"
+BINDING_NAME_READYCHECK = "Initiate Ready Check"
+BINDING_NAME_PULLTIMER = "Initiate Pull Timer"
+BINDING_NAME_CPULLTIMER = "Cancel Pull Timer"
